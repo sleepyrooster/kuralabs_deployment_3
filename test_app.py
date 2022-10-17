ed10c2cbce4149e785434703ebcd695d3ef45270
Walter Emmanuel <53017792+sleepyrooster@users.noreply.github.com>
2022-10-16 21:41:26 -0400

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -5,6 +5,6 @@ def test_quick():
   greeting = greet(a)
   assert greeting == "Hi jeff"
 
-#  def test_home_page():
-#      response = app.test_client().get('/')
-#      assert response.status_code == 200
+def test_home_page():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
